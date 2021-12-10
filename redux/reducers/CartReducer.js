552731e724303d1ebe1edd48c5d86f0cde9f0ecd
Mahendra Gohil <mgohil984@rku.ac.in>
2021-12-10 17:51:29 +0530

    setting up a ci/cd distribution

--- redux/reducers/CartReducer.js
+++ redux/reducers/CartReducer.js
@@ -4,45 +4,51 @@
 import { stat } from "react-native-fs";
 import {
 
-
+ 
     ADD_TO_CART_FAILED,
-    ADD_TO_CART_REQUEST,
-    ADD_TO_CART_SUCCESS,
+    ADD_TO_CART_REQUEST, 
+    ADD_TO_CART_SUCCESS, 
     FETCH_CART_FAILED,
-    FETCH_CART_REQUEST,
+    FETCH_CART_REQUEST, 
     FETCH_CART_SUCCESS
-} from "../Types/CartTypes";
+  } from "../Types/CartTypes";
 
 
 
-const initialstate = {
+const initialstate={
+    
 
-
-    Cart: [],
-    total: 0,
-    totalPrice: 0,
-    loading: false,
-
+    Cart:[],
+    total:0,
+    totalPrice:0,
+    loading:false,
+    
 }
 
 
-const CartReducer = (state = initialstate, action) => {
+const CartReducer=(state=initialstate,action)=>
 
-    switch (action.type) {
+
+{
+
+    switch(action.type)
+    {
         case FETCH_CART_REQUEST:
-            return { ...state, loading: true }
+           return {...state,loading:true}
         case FETCH_CART_SUCCESS:
-
-            return Object.assign({}, state, { Cart: data.Cart, total: data.total, totalPrice: data.totalprice, loading: false })
+             const data=action.payload
+             return Object.assign({},state,{Cart:data.Cart,total:data.total,totalPrice:data.totalprice,loading:false})
         case FETCH_CART_FAILED:
-            return Object.assign({}, state, { loading: false })
+             return Object.assign({},state,{loading:false})
         case ADD_TO_CART_REQUEST:
-
+            console.log('added in cart')
         case ADD_TO_CART_SUCCESS:
-
+            console.log(action.payload+"SUCCED PAYLOAD")
+            
         case ADD_TO_CART_FAILED:
+            console.log(JSON.stringify(action.payload)+"Cart add ERROR")
 
-
+            
         default:
             return state
 
